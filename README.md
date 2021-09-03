278d1fb012ec0fd137278b02042de858c7d4940a
banu aria <banuaria@gmail.com>
2021-09-03 23:39:39 +0700

    success

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # frontend-donasi
 
+## what we got here?
+- Tailwindcss
+
 ## Project setup
 ```
 npm install
@@ -20,5 +23,3 @@ npm run build
 npm run lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+
+import auth from './module/auth'
+import donation from './module/donation'
+import profile from './module/profile'
+import slider from './module/slider'
+import category from './module/category'
+import campaign from './module/campaign'
+
+export default createStore({
+
+    modules: {
+        auth,       // <-- module auth
+        donation,   // <-- module donation
+        profile,
+        slider,
+        category,
+        campaign,
+    }
+
+})
