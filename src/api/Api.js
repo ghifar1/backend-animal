f6bc928ed163b89735cb1f952c05f4c3c9dec568
banu aria <banuaria@gmail.com>
2021-09-05 21:24:41 +0700

    initial commit

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'http://backend-donasi.herokuapp.com/api'
 })
 
 export default Api
